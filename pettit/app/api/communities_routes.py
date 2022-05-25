04ca98a23dd0218e6c1c3281f985dc8c10c7f0e0
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-05-24 22:28:47 -0700

    added add community backend route

--- pettit/app/api/communities_routes.py
+++ pettit/app/api/communities_routes.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from urllib import request
 from flask import Blueprint
 from app.models import Communities, Post, db
 
@@ -9,3 +11,19 @@ def get_communities():
     communities = Communities.query.all()
 
     return {'communities': [community.to_dict() for community in communities]}
+
+@community_routes.route('/new', methods=["POST"])
+def create_community():
+    data = request.json
+
+    community_name = data["community_name"]
+    community_description = data["community_description"]
+
+    new_community = Communities(
+        community_name=community_name,
+        community_description=community_description,
+    )
+    db.session.add(new_community)
+    db.session.commit()
+
+    return new_community.to_dict()
