b4a423b354dc1ee01986f5c138630ac0f153a535
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-05-26 18:38:23 -0700

    added input field on modal

--- pettit/react-app/src/components/CreateCommunityModal/index.js
+++ pettit/react-app/src/components/CreateCommunityModal/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from '../../Context/Modal';
 import './createCommModal.css';
 
 function CreatCommunityModal({ showCreateModal, setShowCreateModal }) {
+    const [communityName, setCommunityName] = useState('p/');
+    console.log(communityName)
+
     return (
         <Modal onClose={() => setShowCreateModal(false)}>
             <div className='create-comm-div'>
@@ -11,14 +14,15 @@ function CreatCommunityModal({ showCreateModal, setShowCreateModal }) {
                     <button>X</button>
                 </div>
                 <form>
+                    <h3>Name</h3>
+                    <h6>Community names including capitalization cannot be changed.</h6>
                     <input
                         type='text'
                         name='communityName'
-                        value={`/p${""}`}
-                        onChange={e => e.target.value}
-                        placeholder={"p/"}
-                    />
-                    
+                        value={communityName}
+                        onChange={e => setCommunityName(e.target.value)}
+                        // placeholder={"p/"}
+                    ></input> 
                 </form>
             </div>
         </Modal>
