475991c92c9eea7a8966d6fdc5adeff762e1fab1
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-05-26 22:58:55 -0700

    styling

--- pettit/react-app/src/components/CreateCommunityModal/index.js
+++ pettit/react-app/src/components/CreateCommunityModal/index.js
@@ -39,21 +39,21 @@ function CreatCommunityModal({ showCreateModal, setShowCreateModal }) {
                                 type='radio'
                                 value={`21 ${""} Public`}
                             />
-                            <BsFillPersonFill className='type-icon'/><span className='comm-types-label'> Public</span><span> Anyone can view, post, and comment to this community</span>
+                            <BsFillPersonFill className='type-icon'/><span className='comm-types-label'> Public</span><span className='type-description'> Anyone can view, post, and comment to this community</span>
                         </div>
                         <div className='type-of-comm'>
                             <input
                                 type='radio'
                                 value={`${""} Public`}
                             />
-                            <BsEye className='type-icon'/><span className='comm-types-label'> Restricted</span><span> Anyone can view this community, but only approved users can post</span>
+                            <BsEye className='type-icon'/><span className='comm-types-label'> Restricted</span><span className='type-description'> Anyone can view this community, but only approved users can post</span>
                         </div>
                         <div className='type-of-comm'>
                             <input
                                 type='radio'
                                 value={`${""} Public`}
                             />
-                            <IoMdLock className='type-icon'/><span className='comm-types-label'> Private</span><span> Only approved users can view and submit to this community</span>
+                            <IoMdLock className='type-icon'/><span className='comm-types-label'> Private</span><span className='type-description'> Only approved users can view and submit to this community</span>
                         </div>
                     </div>
                 </form>
