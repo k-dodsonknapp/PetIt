7e60a7b49e97aca1afa2ebbe91f868cf6407fc68
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-05-26 18:30:11 -0700

    creating create community form

--- pettit/react-app/src/components/CreateCommunityModal/index.js
+++ pettit/react-app/src/components/CreateCommunityModal/index.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { Modal } from '../../Context/Modal';
 import './createCommModal.css';
 
-function CreatCommunityModal({showCreateModal, setShowCreateModal}) {
-  return (
-      <Modal onClose={() => setShowCreateModal(false)}>
-          <form>
-
-          </form>
-          <div className='create-comm-div'>CreatCommunityModal</div>
-      </Modal>
-  )
+function CreatCommunityModal({ showCreateModal, setShowCreateModal }) {
+    return (
+        <Modal onClose={() => setShowCreateModal(false)}>
+            <div className='create-comm-div'>
+                <div>
+                    <p>Create a Community</p>
+                    <button>X</button>
+                </div>
+                <form>
+                    <input
+                        type='text'
+                        name='communityName'
+                        value={`/p${""}`}
+                        onChange={e => e.target.value}
+                        placeholder={"p/"}
+                    />
+                    
+                </form>
+            </div>
+        </Modal>
+    )
 }
 
 export default CreatCommunityModal;
