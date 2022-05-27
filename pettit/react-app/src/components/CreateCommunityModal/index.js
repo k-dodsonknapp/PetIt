f9f12bbfaf465a19dae31cf051a7c839f774a008
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-05-26 19:45:45 -0700

    created classnames for modal

--- pettit/react-app/src/components/CreateCommunityModal/index.js
+++ pettit/react-app/src/components/CreateCommunityModal/index.js
@@ -9,17 +9,16 @@ function CreatCommunityModal({ showCreateModal, setShowCreateModal }) {
     return (
         <Modal onClose={() => setShowCreateModal(false)}>
             <div className='create-comm-div'>
-                <div>
+                <div className='modal-label'>
                     <p>Create a Community</p>
                     <button>X</button>
                 </div>
                 <form>
-                    <div>
+                    <div className='comm-name-label'>
                         <h3>Name</h3>
                         <h6>Community names including capitalization cannot be changed.</h6>
                     </div>
-                    <div>
-
+                    <div className='comm-name-input'>
                         <input
                             type='text'
                             name='communityName'
@@ -31,26 +30,30 @@ function CreatCommunityModal({ showCreateModal, setShowCreateModal }) {
                     </div>
                     <div className='community-type'>
                         <h4>Community Type</h4>
-                        <div>
+                        <div className='public-comm-div'>
                             <input
                                 type='radio'
                                 value={`${""} Public`}
                             />
-                            <span> Public</span>
+                            <span> Public</span><span> Anyone can view, post, and comment to this community</span>
                         </div>
-                        <div>
+                        <div className='restrict-comm-div'>
                             <input
                                 type='radio'
                                 value={`${""} Public`}
                             />
-                            <span> Restricted</span>
+                            <span> Restricted</span><span> Anyone can view this community, but only approved users can post</span>
                         </div>
-                        <div>
+                        <div className='private-comm-div'>
                             <input
                                 type='radio'
                                 value={`${""} Public`}
                             />
-                            <span> Private</span>
+                            <span> Private</span><span> Only approved users can view and submit to this community</span>
+                        </div>
+                        <div className='cancel-create-btn-div'>
+                            <button>Cancel</button>
+                            <button>Create Community</button>
                         </div>
                     </div>
                 </form>
